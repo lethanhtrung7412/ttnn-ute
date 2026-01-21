d13a52720cfc7cc5b12d71824fafa836cb21ef83
Trung Le <lethanhtrung7412@gmail.com>
2026-01-21 14:24:06 +0700

    setup github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: '/'
+  base: '/ttnn-ute/'
 })
